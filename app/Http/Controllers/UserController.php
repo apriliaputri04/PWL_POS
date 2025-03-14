da692d267e06c4966fcd8001286c0ab193f1a2c6
apriliaputri04 <apriliaputri8057@gmail.com>
2025-03-15 04:39:21 +0700

    J4 P2.7 L2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,11 +8,11 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function index()
-    {
-        $user = UserModel::all();
-        return view('user', ['data' => $user]);
-    }
+    //public function index()
+    //{
+      //  $user = UserModel::all();
+       // return view('user', ['data' => $user]);
+    //}
 
     public function tambah()
     {
@@ -53,5 +53,10 @@ class UserController extends Controller
         $user->delete();               // Menghapus user dari database
     
         return redirect('/user');      // Redirect kembali ke halaman daftar user
+    }
+
+    public function index() {
+        $user = UserModel::with('level')->get();
+        dd($user);
     }
 }
