ff38bdd5a947adf710d4437ca3f2dcfa4688d6a5
apriliaputri04 <apriliaputri8057@gmail.com>
2025-03-15 02:13:09 +0700

    J4 P2.4 L10

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::firstOrNew(
+        $user = UserModel::firstOrCreate(
             [
                 'username' => 'manager33',
                 'nama' => 'Manager Tiga Tiga',
