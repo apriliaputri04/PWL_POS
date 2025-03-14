fc385e949d4f227803c8c36dd3ec9babe133dc96
apriliaputri04 <apriliaputri8057@gmail.com>
2025-03-15 02:18:49 +0700

    J4 P2.5 L1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,22 +3,37 @@
 namespace App\Http\Controllers;
 
 use App\Models\UserModel;
-//use Illuminate\Http\Request;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::firstOrCreate(
-            [
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
-                'password' => Hash::make('12345'),
-                'level_id' => 2
+        $user = UserModel::create([
+            'username' => 'manager55',
+            'nama' => 'Manager55',
+            'password' => Hash::make("12345"),
+            'level_id' => 2,
+        ]);
 
-            ]
-        );
-        return view('user', ['data' => $user]);
-    }    
+        $user->username = 'manager56';
+
+        $user->isDirty(); // true
+        $user->isDirty('username'); // true
+        $user->isDirty('nama'); // false
+        $user->isDirty(['nama', 'username']); // true
+
+        $user->isClean(); // false
+        $user->isClean('username'); // false
+        $user->isClean('nama'); // true
+        $user->isClean(['nama', 'username']); // false
+
+        $user->save();
+
+        $user->isDirty(); // false
+        $user->isClean(); // true
+
+        dd($user->isDirty());
+    }
 }
