6cc0e4058be0935a461e1b4371d931b0592cae60
apriliaputri04 <apriliaputri8057@gmail.com>
2025-03-14 23:47:37 +0700

    J4 P1 L4

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -16,5 +16,5 @@ class UserModel extends Model
      * The attributes that are mass assignable
      * @var array
      */
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    protected $fillable = ['level_id', 'username', 'nama'];
 }
