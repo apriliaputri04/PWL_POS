09a550fcede4d3568ad38cbeba3db48be331158f
apriliaputri04 <apriliaputri8057@gmail.com>
2025-03-15 00:59:56 +0700

    J4 P2.3 L1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,8 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::findOrFail(1);
+        $user = UserModel::where('level_id', 2)->count();
+        dd($user);
         return view('user', ['data' => $user]);
     }
 }
