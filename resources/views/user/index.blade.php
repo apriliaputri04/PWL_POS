4ac4c5d21641f76f9c4544fc1ebaf8986531382a
apriliaputri04 <apriliaputri8057@gmail.com>
2025-03-20 02:29:15 +0700

    Perbaikan fix

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,25 +1,31 @@
 @extends('layouts.template')
 
-    @if (session('error'))
-        <div class="alert alert-danger">{{ session('error') }}</div>
-    @endif
-    <div class="row">
-         <div class="col-md-12">
-            <div class="form-group row">
-                <label class="col-1 control-label col-form-label">Filter:</label>
-                <div class="col-3">
-                    <select class="form-control" id="level_id" name="level_id" required>
-                        <option value="">Semua</option>
-                        @foreach ($level as $item)
-                            <option value="{{ $item->level_id }}">{{ $item->level_nama }}</option>
-                         @endforeach
-                        </select>
-                        <small class="form-text text-muted">Level Pengguna</small>
-                    </div>
-                </div>
+@section('content')
+
+@if (session('error'))
+    <div class="alert alert-danger">{{ session('error') }}</div>
+@endif
+
+<div class="row mb-3">
+    <div class="col-md-6">
+        <div class="form-group row">
+            <label class="col-3 control-label col-form-label">Filter:</label>
+            <div class="col-6">
+                <select class="form-control" id="level_id" name="level_id">
+                    <option value="">Semua</option>
+                    @foreach ($level as $item)
+                        <option value="{{ $item->level_id }}">{{ $item->level_nama }}</option>
+                    @endforeach
+                </select>
+                <small class="form-text text-muted">Jenis Pengguna</small>
             </div>
         </div>
-        <table class="table table-bordered table-striped table-hover table-sm" id="table_user">
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-md-12">
+        <table class="table table-bordered table-striped table-hover table-sm" id="table_user" width="100%">
             <thead>
                 <tr>
                     <th>ID</th>
@@ -30,56 +36,48 @@
                 </tr>
             </thead>
         </table>
-    </div>  
+    </div>
+</div>
+
 @endsection
 
 @push('css')
+<style>
+    /* Agar tabel tetap rapi */
+    #table_user {
+        width: 100% !important;
+    }
+    .dataTables_filter {
+        float: right;
+    }
+</style>
 @endpush
 
 @push('js')
 <script>
     $(document).ready(function() {
         var dataUser = $('#table_user').DataTable({
+            processing: true,
             serverSide: true,
+            responsive: true,
+            autoWidth: false,
+            layout: 'fixed',
             ajax: {
                 url: "{{ url('user/list') }}",
-                dataType: "json",
-                type: "POST"
-                "data" : function (d) {
+                type: "POST",
+                data: function(d) {
                     d.level_id = $("#level_id").val();
+                },
+                headers: {
+                    'X-CSRF-TOKEN': "{{ csrf_token() }}"
                 }
             },
             columns: [
-                {
-                    data: "DT_RowIndex",
-                    className: "text-center",
-                    orderable: false,
-                    searchable: false
-                },
-                {
-                    data: "username",
-                    className: "",
-                    orderable: true,
-                    searchable: true
-                },
-                {
-                    data: "nama",
-                    className: "",
-                    orderable: true,
-                    searchable: true
-                },
-                {
-                    data: "level.level_nama",
-                    className: "",
-                    orderable: false,
-                    searchable: false
-                },
-                {
-                    data: "aksi",
-                    className: "",
-                    orderable: false,
-                    searchable: false
-                }
+                { data: "DT_RowIndex", className: "text-center", orderable: false, searchable: false },
+                { data: "username", orderable: true, searchable: true },
+                { data: "nama", orderable: true, searchable: true },
+                { data: "level.level_nama", orderable: false, searchable: false },
+                { data: "aksi", orderable: false, searchable: false }
             ]
         });
 
