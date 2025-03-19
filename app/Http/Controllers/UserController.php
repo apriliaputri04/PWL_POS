b93d52b2b00e08af8ce692986713bc7dd9e9e85d
apriliaputri04 <apriliaputri8057@gmail.com>
2025-03-20 02:17:30 +0700

    J5 P4 L5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -39,6 +39,11 @@ public function list(Request $request)
     $users = UserModel::select('user_id', 'username', 'nama', 'level_id')
         ->with('level');
 
+    // filter data user berdasarkan level_id
+    if ($request->level_id) {
+        $users->where('level_id', $request->level_id);
+    }   
+
     return DataTables::of($users)
         // Menambahkan kolom index / nomor urut
         ->addIndexColumn()
