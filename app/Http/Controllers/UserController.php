e37d36704f05e02377739d9858e72191d7fc6e97
apriliaputri04 <apriliaputri8057@gmail.com>
2025-03-19 21:40:50 +0700

    J5 P3 L4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,67 +1,27 @@
 <?php
-
 namespace App\Http\Controllers;
 
+use App\Models\LevelModel;
 use App\Models\UserModel;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
+use Yajra\DataTables\Facades\DataTables;
 
 class UserController extends Controller
 {
-    //public function index()
-    //{
-      //  $user = UserModel::all();
-       // return view('user', ['data' => $user]);
-    //}
-
-    public function tambah()
+    // Menampilkan halaman awal user
+    public function index()
     {
-        return view('user_tambah');
-    }
+        $breadcrumb = (object) [
+            'title' => 'Daftar User',
+            'list' => ['Home', 'User']
+        ];
 
-    public function tambah_simpan(Request $request) {
-        UserModel::create([
-            'username' => $request->username,
-            'nama' => $request->nama,
-            'password' => Hash::make($request->password),
-            'level_id' => $request->level_id
-        ]);
-    
-        return redirect('/user');
-    }
+        $page = (object) [
+            'title' => 'Daftar user yang terdaftar dalam sistem'
+        ];
 
-    public function ubah($id)
-    {
-        $user = UserModel::find($id);
-        return view('user_ubah', ['data' => $user]);
-    }
+        $activeMenu = 'user'; // set menu yang sedang aktif
 
-    public function ubah_simpan($id, Request $request) {
-        $user = UserModel::find($id);
-    
-        $user->username = $request->username;
-        $user->nama = $request->nama;
-        $user->password = Hash::make('$request->password');
-        $user->level_id = $request->level_id;
-        $user->save();
-    
-        return redirect('/user');
-    }
-
-    public function hapus($id) {
-        $user = UserModel::find($id);  // Mencari user berdasarkan ID
-        $user->delete();               // Menghapus user dari database
-    
-        return redirect('/user');      // Redirect kembali ke halaman daftar user
-    }
-
-    //public function index() {
-      //  $user = UserModel::with('level')->get();
-        //dd($user);
-    //}
-
-    public function index() {
-        $user = UserModel::with('level')->get();
-        return view('user', ['data' => $user]);
+        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu]);
     }
 }
