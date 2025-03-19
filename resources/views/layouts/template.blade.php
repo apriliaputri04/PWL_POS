803315f2b504fecd7af34f92421811b2a0464bbc
apriliaputri04 <apriliaputri8057@gmail.com>
2025-03-19 22:44:04 +0700

    perbaikan

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name', 'PWL Laravel Starter Code') }}</title>
 
-    <meta name="csrf-token" content="{{ csrf_token}}"> <!-- Untuk mengirimkan token Laravel CSRF pada setiap request ajax-->
+    <meta name="csrf-token" content="{{ csrf_token()}}"> <!-- Untuk mengirimkan token Laravel CSRF pada setiap request ajax-->
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
