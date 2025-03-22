786a72622733811d649e0a15360e4a38d1d95288
apriliaputri04 <apriliaputri8057@gmail.com>
2025-03-22 20:26:04 +0700

    J06 P1 L4

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -42,6 +42,9 @@
             </table>
         </div>
     </div>
+    <!-- Modal -->
+    <div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" data-
+        backdrop="static" data-keyboard="false" data-width="75%" aria-hidden="true"></div>
 @endsection
 
 @push('css')
