656c94ba349f268524e018dbb098476ecf769d27
apriliaputri04 <apriliaputri8057@gmail.com>
2025-04-27 00:05:43 +0700

    P3 +logo polinema dan ubah di export

--- resources/views/barang/export_pdf.blade.php
+++ resources/views/barang/export_pdf.blade.php
@@ -61,7 +61,7 @@
     <table class="border-bottom-header">
         <tr>
             <td width="15%" class="text-center">
-                <img src="{{ asset('polinema-bw.png') }}" class="image">
+                <img src="{{ asset('logopolinema.png') }}" class="image">
             </td>
             <td width="85%">
                 <span class="text-center d-block font-11 font-bold mb-1">
