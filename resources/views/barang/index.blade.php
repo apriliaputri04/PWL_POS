e570344f6e5d66dcd7f8fc861a6439298f775227
apriliaputri04 <apriliaputri8057@gmail.com>
2025-04-26 19:41:08 +0700

    J08 P1 L3 FIX

--- resources/views/barang/index.blade.php
+++ resources/views/barang/index.blade.php
@@ -1,24 +1,17 @@
 @extends('layouts.template')
-@section('content')
 
+@section('content')
 <div class="card">
     <div class="card-header">
-        <h3 class="card-title">Daftar Barang</h3>
+        <h3 class="card-title">Daftar barang</h3>
         <div class="card-tools">
-            <button onclick="modalAction('{{ url('/barang/import') }}')" class="btn btn-info">
-                Import Barang
-            </button>
-            <a href="{{ url('/barang/create') }}" class="btn btn-primary">
-                Tambah Data
-            </a>
-            <button onclick="modalAction('{{ url('/barang/create_ajax') }}')" class="btn btn-success">
-                Tambah Data (Ajax)
-            </button>
+            <button onclick="modalAction('{{ url('/barang/import') }}')" class="btn btn-info">Import Barang</button>
+            <a href="{{ url('/barang/create') }}" class="btn btn-primary">Tambah Data</a>
+            <button onclick="modalAction('{{ url('/barang/create_ajax') }}')" class="btn btn-success">Tambah Data (Ajax)</button>
         </div>
     </div>
-
     <div class="card-body">
-        <!-- Filter Data -->
+        <!-- untuk Filter data -->
         <div id="filter" class="form-horizontal filter-date p-2 border-bottom mb-2">
             <div class="row">
                 <div class="col-md-12">
@@ -37,15 +30,15 @@
                 </div>
             </div>
         </div>
-
+        
         @if(session('success'))
             <div class="alert alert-success">{{ session('success') }}</div>
         @endif
-
+        
         @if(session('error'))
             <div class="alert alert-danger">{{ session('error') }}</div>
         @endif
-
+        
         <table class="table table-bordered table-sm table-striped table-hover" id="table-barang">
             <thead>
                 <tr>
@@ -63,95 +56,97 @@
     </div>
 </div>
 
-<div id="myModal" class="modal fade animate shake" tabindex="-1" data-backdrop="static"
-    data-keyboard="false" data-width="75%"></div>
-
-@endsection
+<div id="myModal" class="modal fade animate shake" tabindex="-1" data-backdrop="static" data-keyboard="false" data-width="75%"></div>
+@endsection 
 
 @push('js')
 <script>
-function modalAction(url = '') {
-    $('#myModal').load(url, function() {
-        $('#myModal').modal('show');
-    });
-}
-
-var tableBarang;
-$(document).ready(function() {
-    tableBarang = $('#table-barang').DataTable({
-        processing: true,
-        serverSide: true,
-        ajax: {
-            url: "{{ url('barang/list') }}",
-            dataType: "json",
-            type: "POST",
-            data: function (d) {
-                d.filter_kategori = $('.filter_kategori').val();
-            }
-        },
-        columns: [
-            {
-                data: "No_Urut",
-                className: "text-center",
-                width: "5%",
-                orderable: false,
-                searchable: false
-            },
-            {
-                data: "barang_kode",
-                width: "10%",
-                orderable: true,
-                searchable: true
-            },
-            {
-                data: "barang_nama",
-                width: "37%",
-                orderable: true,
-                searchable: true
-            },
-            {
-                data: "harga_beli",
-                width: "10%",
-                orderable: true,
-                searchable: false,
-                render: function(data) {
-                    return new Intl.NumberFormat('id-ID').format(data);
+    function modalAction(url = '') {
+        $('#myModal').load(url, function() {
+            $('#myModal').modal('show');
+        });
+    }
+    var tableBarang;
+    $(document).ready(function() {
+        tableBarang = $('#table-barang').DataTable({
+            processing: true,
+            serverSide: true, 
+            ajax: {
+                "url": "{{ url('barang/list') }}",
+                "dataType": "json",
+                "type": "POST",
+                "data": function (d) {
+                    d.filter_kategori = $('.filter_kategori').val();
                 }
             },
-            {
-                data: "harga_jual",
-                width: "10%",
-                orderable: true,
-                searchable: false,
-                render: function(data) {
-                    return new Intl.NumberFormat('id-ID').format(data);
+            columns: [
+                {
+                    data: "DT_RowIndex",
+                    className: "text-center",
+                    width: "5%",
+                    orderable: false,
+                    searchable: false
+                },
+                {
+                    data: "barang_kode",
+                    className: "",
+                    width: "10%",
+                    orderable: true,
+                    searchable: true
+                },
+                {
+                    data: "barang_nama",
+                    className: "",
+                    width: "37%",
+                    orderable: true,
+                    searchable: true
+                },
+                {
+                    data: "harga_beli",
+                    className: "",
+                    width: "10%",
+                    orderable: true,
+                    searchable: false,
+                    render: function(data, type, row) {
+                        return new Intl.NumberFormat('id-ID').format(data);
+                    }
+                },
+                {
+                    data: "harga_jual",
+                    className: "",
+                    width: "10%",
+                    orderable: true,
+                    searchable: false,
+                    render: function(data, type, row) {
+                        return new Intl.NumberFormat('id-ID').format(data);
+                    }
+                },
+                {
+                    data: "kategori.kategori_nama",
+                    className: "",
+                    width: "14%",
+                    orderable: true,
+                    searchable: false
+                },
+                {
+                    data: "aksi",
+                    className: "text-center",
+                    width: "14%",
+                    orderable: false,
+                    searchable: false
                 }
-            },
-            {
-                data: "kategori.kategori_nama",
-                width: "14%",
-                orderable: true,
-                searchable: false
-            },
-            {
-                data: "aksi",
-                className: "text-center",
-                width: "14%",
-                orderable: false,
-                searchable: false
+            ]
+        });
+
+        $('#table-barang_filter input').unbind().bind().on('keyup', function(e) {
+            if (e.keyCode == 13) { // enter key
+                tableBarang.search(this.value).draw();
             }
-        ]
-    });
+        });
 
-    $('#table-barang_filter input').unbind().bind().on('keyup', function(e) {
-        if (e.keyCode == 13) { // Enter key
-            tableBarang.search(this.value).draw();
-        }
+        $('.filter_kategori').change(function() {
+            tableBarang.draw();
+        });
     });
-
-    $('.filter_kategori').change(function() {
-        tableBarang.draw();
-    });
-});
 </script>
 @endpush
